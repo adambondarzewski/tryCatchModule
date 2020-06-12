768b12448b4a4b0a0ce51e7c3486b453452b56ce
Adam Bondarzewski <adam.bondarzewski@contractors.roche.com>
2020-06-12 11:46:08 +0200

    test error added

--- server.R
+++ server.R
@@ -4,6 +4,7 @@ counter <- function(input, output, session) {
   count <- reactiveVal(0)
   observeEvent(input$button, {
     count(count() + 1)
+    # stop("Test observeEvent error")
   })
   output$out <- renderText({
     count()
